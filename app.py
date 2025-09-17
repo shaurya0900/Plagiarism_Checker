9eae65f120f3171c97b59f045c92b7ee35556655
shaurya0900 <shaurya2929@gmail.com>
2025-09-18 03:06:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,33 +2,22 @@ import os
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
+# Read all text files in the directory
 student_files = [doc for doc in os.listdir() if doc.endswith('.txt')]
-student_notes = [open(_file, encoding='utf-8').read()
-                 for _file in student_files]
+student_notes = [open(file, encoding='utf-8').read() for file in student_files]
 
+# Vectorize all documents
+vectors = TfidfVectorizer().fit_transform(student_notes).toarray()
 
-def vectorize(Text): return TfidfVectorizer().fit_transform(Text).toarray()
-def similarity(doc1, doc2): return cosine_similarity([doc1, doc2])
+# Check pairwise similarity
+plagiarism_results = []
 
+for i in range(len(student_files)):
+    for j in range(i + 1, len(student_files)):
+        sim_score = cosine_similarity([vectors[i], vectors[j]])[0][1]
+        plagiarism_results.append((student_files[i], student_files[j], sim_score))
 
-vectors = vectorize(student_notes)
-s_vectors = list(zip(student_files, vectors))
-plagiarism_results = set()
+# Print results
+for result in plagiarism_results:
+    print(result)
 
-
-def check_plagiarism():
-    global s_vectors
-    for student_a, text_vector_a in s_vectors:
-        new_vectors = s_vectors.copy()
-        current_index = new_vectors.index((student_a, text_vector_a))
-        del new_vectors[current_index]
-        for student_b, text_vector_b in new_vectors:
-            sim_score = similarity(text_vector_a, text_vector_b)[0][1]
-            student_pair = sorted((student_a, student_b))
-            score = (student_pair[0], student_pair[1], sim_score)
-            plagiarism_results.add(score)
-    return plagiarism_results
-
-
-for data in check_plagiarism():
-    print(data)
